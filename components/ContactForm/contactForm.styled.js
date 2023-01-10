b2d230e3ff6299dd82cf642be2e5d865b0edd940
Elsa <elsahvy@gmail.com>
2023-01-10 12:52:39 -0800

    changed project sq, contact form styles

--- components/ContactForm/contactForm.styled.js
+++ components/ContactForm/contactForm.styled.js
@@ -3,7 +3,7 @@ import styled from "styled-components";
 export const Wrapper = styled.div`
   display: flex;
   justify-content: flex-start;
-  color: #000;
+  // color: #000;
   // background: green;
 `;
 
