6d9bc558e2eb112b7a09a0377c4b703efb33f177
Elsa <elsahvy@gmail.com>
2023-02-15 15:29:50 -0800

    revised aside timeline content

--- components/Aside/index.js
+++ components/Aside/index.js
@@ -18,10 +18,7 @@ const Aside = () => {
             Hi there, {"I'm"} Elsa. {"I'm"} a web developer based in Joshua
             Tree, CA.
           </p>
-          {/* <p>
-            I build landing pages for small businesses and creatives starting
-            out.
-          </p> */}
+
           <ImageWrapper>
             <Image src={PortfolioPic} alt="elsa hovey" layout="responsive" />
           </ImageWrapper>
@@ -34,22 +31,29 @@ const Aside = () => {
                 <h2 style={{ paddingBottom: "10px" }}>
                   A little bit about me...
                 </h2>
-                <TimelineDate> 2020-2022:</TimelineDate>
+                <TimelineDate> 2020-2023:</TimelineDate>
                 <p>
                   Taught myself programming and web design through various (and
                   super helpful!) youtube developers. Also utilized other
                   resources such as: freecodecamp, the odin project, geeks for
                   geeks.
                 </p>
-                <p>Spent this time building my featured projects</p>
+                <p>
+                  Spent this time, and am currently building up my featured
+                  projects
+                </p>
               </li>
               <li>
                 <TimelineDate> 2016-2020:</TimelineDate>
+                <p>Worked in restaurants and assisted living kitchens.</p>
                 <p>
-                  Worked in restaurants and assisted living kitchens. Worked as
-                  a Dietary Manager from 2018-2020.... loved it until I desired
-                  a change, mainly for finding more creative employment. Became
-                  interested with programming and started building my skills.
+                  Worked as a Dietary Manager from 2018-2020.... loved it until
+                  I desired a change, mainly for finding more creative
+                  employment.
+                </p>
+                <p>
+                  In 2019, I became interested with programming and started
+                  learning Javascript.
                 </p>
               </li>
               <li>
